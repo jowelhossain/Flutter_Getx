65e9beaf0dff209b81311e071ef878a9ff5654d9
Jowel Hossain <jowelhossain117@gmail.com>
2023-08-26 12:14:37 +0600

    slider with getx

--- lib/screen/slider.dart
+++ lib/screen/slider.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SliderTestController extends GetxController{
+
+RxDouble opacity= 0.4.obs;
+
+setOpacity(double value){
+  opacity.value=value;
+}
+}
 
 class SliderTest extends StatefulWidget {
   const SliderTest({super.key});
@@ -10,25 +20,25 @@ class SliderTest extends StatefulWidget {
 }
 
 class _SliderTestState extends State<SliderTest> {
+
+  SliderTestController sliderTestController=Get.put(SliderTestController());
   double opacity=1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Slider with GetX"),),
       body: Column(children: [
-        Container(
-          height: 100,
-          width: 100,
-          color: Colors.red.withOpacity(opacity)
+        Obx(()=>Container(
+            height: 100,
+            width: 100,
+            color: Colors.red.withOpacity(sliderTestController.opacity.value)
 
+          ),
         ),
-        Slider(value: opacity, onChanged: (value){
+        Obx(() =>Slider(value: sliderTestController.opacity.value, onChanged: (value){
 
-          opacity=value;
-          setState(() {
-
-          });
-        })
+        sliderTestController.setOpacity(value);
+        }))
 
       ],),
     );
